384315ba41f0fea52b0c49520361de01d6dad1b7
Tanya Sonker <tsonker@scu.edu>
2023-01-14 22:58:44 +0530

    setting up app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+
+  constructor(private translateService: TranslateService){
+    // fallback when translation not found in current lang
+    this.translateService.setDefaultLang('en');
+    // current language to use
+    this.translateService.use('en');
+  }
+
+  changeLanguage(lang: string){
+    this.translateService.use(lang);
+  }
 }
